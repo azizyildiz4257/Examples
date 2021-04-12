d7fb6679318c8428148c661a2338541f9633b9b1
azizyildiz4257 <azizyildiz4257@hotmail.com>
2021-04-12 18:05:23 +0300

    Add project files.

---
+++ Dictionary/MyDictionary.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dictionary
+{
+    class MyDictionary<T_key, T_value>
+    {
+        T_key[] Key;
+        T_value[] Value;
+
+        public MyDictionary()
+        {
+            Key = new T_key[0];
+            Value = new T_value[0];
+        }
+        public void Add(T_key key, T_value value)
+        {
+            T_key[] tempkey = Key;
+            T_value[] tempvalue = Value;
+
+            Key = new T_key[Key.Length + 1];
+            Value = new T_value[Value.Length + 1];
+
+            for (int i = 0; i < tempkey.Length; i++)
+            {
+                Key[i] = tempkey[i];
+            }
+            Key[Key.Length - 1] = key;
+
+            for (int i = 0; i < tempvalue.Length; i++)
+            {
+                Value[i] = tempvalue[i];
+            }
+            Value[Value.Length - 1] = value;
+        }
+        public int Count
+        {
+            get { return Key.Length; }
+        }
+
+        public T_key[] Keys
+        {
+            get { return Key; }
+        }
+
+        public T_value[] Values
+        {
+            get { return Value; }
+        }
+
+    }
+}
---
+++ Dictionary/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> plakalar = new Dictionary<int, string>();
+            plakalar.Add(42, "Konya");
+            plakalar.Add(34, "İstanbul");
+
+            foreach (KeyValuePair<int, string> item in plakalar)
+            {
+                Console.WriteLine("{0} - {1}", item.Key, item.Value);
+            }
+
+            Console.WriteLine("Toplam Kayıt : " + plakalar.Count);
+            Console.WriteLine();
+
+            MyDictionary<int, string> plakalar2 = new MyDictionary<int, string>();
+            plakalar2.Add(38, "Kayseri");
+
+            foreach (var Keys in plakalar2.Keys)
+            {
+                Console.Write(Keys);
+            }
+
+            foreach (var Values in plakalar2.Values)
+            {
+                Console.WriteLine(" - " + Values);
+            }
+
+            Console.WriteLine("Toplam Kayıt : " + plakalar2.Count);
+
+            Console.Read();
+        }
+    }
+}
